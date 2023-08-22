d454610cd09059c3267a61a0cde0c44e7c434230
15868707168@163.com <15868707168@163.com>
2023-08-22 18:03:59 +0800

    更新登录和用户信息接口

--- Applications/Manager.API/Controllers/UsersController.cs
+++ Applications/Manager.API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using Manager.API.Utility.Filters;
 using Manager.Core;
 using Manager.Core.Enums;
 using Manager.Server.IServices;
+using Manager.Server.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,22 +17,55 @@ namespace Manager.API.Controllers
     public class UsersController : ApiController
     {
         private readonly IAccountInfoService accountInfoService;
+        private readonly IAccountService accountService;
         private readonly IUserFocusService userFocusService;
         private readonly IBlogService blogService;
 
-        public UsersController(IAccountInfoService accountInfoService, IUserFocusService userFocusService, IBlogService blogService)
+        public UsersController(IAccountInfoService accountInfoService, IAccountService accountService, IUserFocusService userFocusService, IBlogService blogService)
         {
             this.accountInfoService = accountInfoService;
+            this.accountService = accountService;
             this.userFocusService = userFocusService;
             this.blogService = blogService;
         }
+
+
+        /// <summary>
+        /// 用户信息[当前登录用户]
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> UserInfo()
+        {
+            /*
+             * 1.Account AccountInf
+             * 2.博客数 关注数 粉丝数
+             * 3.关注关系
+             */
+            var wId = UId;
+
+            //1.Account AccountInfo
+            var account = await accountService.GetAccountBy(x => x.UId == wId, false);
+
+            var accountInfo = await accountInfoService.FirstOrDefaultAsync(wId, isCache: true);
+
+            //2.1 博客数量
+            var blogCount = await blogService.GetBlogCountBy(x => x.UId == wId && x.Status == (sbyte)Status.ENABLE);
+
+            //2.2 关注数
+            var focusCount = await userFocusService.GetUserFocusCountBy(x => x.UId == wId);
+
+            //2.3 粉丝数
+            var fanCount = await userFocusService.GetUserFocusCountBy(x => x.BuId == wId);
+
+            return Ok(Success("用户信息获取成功", new { account, accountInfo, blogCount, focusCount, fanCount }));
+        }
+
 
         /// <summary>
         /// 用户信息
         /// </summary>
         /// <param name="uId"></param>
-        /// <param name="wId"></param>
-        /// <param name="params"></param>
         /// <returns></returns>
         [HttpGet("{uId}")]
         public async Task<IActionResult> UserInfo(Guid uId)
@@ -44,9 +78,9 @@ namespace Manager.API.Controllers
             var wId = UId;
 
             //1.Account AccountInfo
-            //var account = await accountService.GetAccountBy(x => x.UId == uId, false);
+            var account = await accountService.GetAccountBy(x => x.UId == uId, false);
 
-            var accountInfo = await accountInfoService.FirstOrDefaultAsync(uId, true);
+            var accountInfo = await accountInfoService.FirstOrDefaultAsync(uId, false);
 
             //2.1 博客数量
             var blogCount = await blogService.GetBlogCountBy(x => x.UId == uId && x.Status == (sbyte)Status.ENABLE);
@@ -60,7 +94,7 @@ namespace Manager.API.Controllers
             //3.关注关系
             var relation = await userFocusService.GetUserFocusBy(x => x.BuId == uId && x.UId == wId);
 
-            return Ok(Success("用户信息获取成功", new { accountInfo, blogCount, focusCount, fanCount, relation }));
+            return Ok(Success("用户信息获取成功", new { account, accountInfo, blogCount, focusCount, fanCount, relation }));
         }
     }
 }
