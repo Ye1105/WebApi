0441ff294ae649734e15d2488af0caf7a63c22ab
15868707168@163.com <15868707168@163.com>
2023-08-24 10:10:27 +0800

    修改账号接口

--- Applications/Manager.API/Controllers/AccountsController.cs
+++ Applications/Manager.API/Controllers/AccountsController.cs
@@ -3,12 +3,15 @@ using Manager.API.Utility.Filters;
 using Manager.API.Utility.Schemas;
 using Manager.Core;
 using Manager.Core.RequestModels;
+using Manager.Extensions;
 using Manager.Server.IServices;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Schema;
+using Org.BouncyCastle.Ocsp;
+using System.Text.RegularExpressions;
 
 namespace Manager.API.Controllers
 {
@@ -27,12 +30,12 @@ namespace Manager.API.Controllers
         }
 
         /// <summary>
-        /// 修改账号
+        /// 修改账号邮箱
         /// </summary>
-        /// <param name="req"></param>
+        /// <param name="mail"></param>
         /// <returns></returns>
-        [HttpPut]
-        public async Task<IActionResult> UpdateAccount([FromBody] UpdateAccountRequest req)
+        [HttpPatch("/mail")]
+        public async Task<IActionResult> UpdateAccountMail([FromForm] string mail)
         {
             /*
              * 0.参数校验
@@ -42,14 +45,52 @@ namespace Manager.API.Controllers
              */
 
             //0.参数校验
-            var jsonSchema = await JsonSchemas.GetSchema("account-update");
-
-            var schema = JSchema.Parse(jsonSchema);
-
-            var validate = JObject.Parse(JsonConvert.SerializeObject(req)).IsValid(schema, out IList<string> errorMessages);
-            if (!validate)
+            if (!Regex.IsMatch(mail, RegexHelper.MailPattern))
             {
-                return Ok(Fail(errorMessages, "参数错误"));
+                return Ok(Fail("邮箱格式不正确", "参数错误"));
+            }
+
+
+            //1.账号是否存在
+            var account = await accountService.GetAccountBy(x => x.UId == UId);
+            if (account == null)
+            {
+                return Ok(Fail("账号不存在"));
+            }
+
+            var accountMail = await accountService.GetAccountBy(x => x.Mail.ToLower() == mail.ToLower() && x.UId != UId, false);
+            if (accountMail != null)
+            {
+                return Ok(Fail("邮箱已存在"));
+            }
+
+            //3.修改账号信息
+            //account.Name = req.Name;
+            account.Mail = mail;
+
+            return await accountService.ModifyAccount(account) ? Ok(Success("修改成功")) : Ok(Fail("修改失败"));
+        }
+
+
+        /// <summary>
+        /// 修改账号手机号
+        /// </summary>
+        /// <param name="phone"></param>
+        /// <returns></returns>
+        [HttpPatch("/phone")]
+        public async Task<IActionResult> UpdateAccountPhone([FromForm] string phone)
+        {
+            /*
+             * 0.参数校验
+             * 1.账号是否存在
+             * 2.判断 mail 是否已经存在
+             * 3.修改邮箱数据
+             */
+
+            //0.参数校验
+            if (!Regex.IsMatch(phone, RegexHelper.PhonePattern))
+            {
+                return Ok(Fail("手机格式不正确", "参数错误"));
             }
 
             //1.账号是否存在
@@ -59,29 +100,14 @@ namespace Manager.API.Controllers
                 return Ok(Fail("账号不存在"));
             }
 
-            //2.判断除当前用户之外的 name mail phone 是否已经有存在
-            //var accountName = await accountService.GetAccountBy(x => x.Name == req.Name && x.UId != UId, false);
-            //if (accountName != null)
-            //{
-            //    return Ok(Fail("账号名称已存在"));
-            //}
-
-            var accountMail = await accountService.GetAccountBy(x => x.Mail.ToLower() == req.Mail.ToLower() && x.UId != UId, false);
-            if (accountMail != null)
-            {
-                return Ok(Fail("邮箱已存在"));
-            }
-
-            var accountPhone = await accountService.GetAccountBy(x => x.Phone == req.Phone && x.UId != UId, false);
+            var accountPhone = await accountService.GetAccountBy(x => x.Phone == phone && x.UId != UId, false);
             if (accountPhone != null)
             {
                 return Ok(Fail("手机号已存在"));
             }
 
             //3.修改账号信息
-            //account.Name = req.Name;
-            account.Mail = req.Mail;
-            account.Phone = req.Phone;
+            account.Phone = phone;
 
             return await accountService.ModifyAccount(account) ? Ok(Success("修改成功")) : Ok(Fail("修改失败"));
         }
