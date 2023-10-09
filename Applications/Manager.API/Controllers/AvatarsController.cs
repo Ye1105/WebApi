6fb40c105c6d046b01e256627f3441b3e224d913
15868707168@163.com <15868707168@163.com>
2023-10-09 17:24:49 +0800

    更新代码

--- Applications/Manager.API/Controllers/AvatarsController.cs
+++ Applications/Manager.API/Controllers/AvatarsController.cs
@@ -98,7 +98,7 @@ namespace Manager.API.Controllers
                 Height = height,
                 Width = width,
                 Created = DateTime.Now,
-                Status = (sbyte)Status.UNDER_REVIEW
+                Status = (sbyte)Status.ENABLE //(sbyte)Status.UNDER_REVIEW
             };
 
             var res = await logAvatarService.AddAsync(logAvatar);
